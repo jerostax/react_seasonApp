e03605bfb84e770276aae92c0dcdea3bf70aa007
jerostax <jeremy.geneste@gmail.com>
2019-06-07 11:49:07 +0200

    [ REFACTO ] Updated App class component as a fx component using hooks

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
-state = { lat: null, errorMessage: '' }
 
-componentDidMount() {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => this.setState({ lat: position.coords.latitude }),
-    (err) =>  this.setState({ errorMessage: err.message })
-  );
-}
-renderContent() {
-  if(this.state.errorMessage && !this.state.lat){
-    return <div>Error : { this.state.errorMessage }</div>;
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => setLat(position.coords.latitude),
+      (err) => setErrorMessage(err.message)
+    );
+  }, []);
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Spinner message="Veuillez accepter la demande de géolocalisation" />;
   }
-  if(!this.state.errorMessage && this.state.lat) {
-    return <SeasonDisplay lat={this.state.lat} />
-  } 
-  return <Spinner message="Veuillez accepter la demande de géolocalisation"/>;
-}
-  // React says we have to define render !!!
-  render(){
-    return (
-      <div style={{border: "10px dotted red", backgroundColor: "orange"}}>
-        {this.renderContent()}
-      </div>
-    )
-  }
-}
+
+  return <div className="border red">{content}</div>
+};
 
 ReactDOM.render(
   <App />,
